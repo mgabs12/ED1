18984d96b25d930c00be263683306284794637b1
mgabs12 <98103601+mgabs12@users.noreply.github.com>
2023-04-25 22:09:15 -0600

    Update Program.cs

--- Laboratorio 3/LAB_NO3/LAB_NO3/Program.cs
+++ Laboratorio 3/LAB_NO3/LAB_NO3/Program.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 namespace LAb02
 {
-
     class Test
     {
 
@@ -49,43 +48,21 @@ namespace LAb02
             string[] arreglo2 = new string[1000];
             int conT = 0;
             int conP = 1;
-            Console.Title = "AVL";
+            Console.Title = "LAB 3 MÓNICA ORTÍZ";
 
-            //string decode = arbolHH.Decodificacion(encode);
-
-            var infodd = "";
-            var info1 = "";
-            var infoN = "";
-            var infoD = "";
-            var infoG = "";
             string[] informacion1 = { };
             string[] informacionP3 = { };
             string[] informacionP2 = { };
             string[] separI22 = { };
-            int c = 0, b = 0, a = 0;
-
-            if (GetUsuarios2() != null && contador == con)
-            {
-                try
-                {
-                    string allFileData = File.ReadAllText(_path2);
-                    foreach (string lineaActual in allFileData.Split('\n'))
-                    {
-                        if (!string.IsNullOrEmpty(lineaActual) && contador == con)
-                        {
-                            conT = 0;
-                            string[] informacion = lineaActual.Split("{");
-
-                            Console.WriteLine("Usuario: " + informacion[1]);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Error" + ex);
-                }
-
-            }
+            string[] Cadena2 = new string[1000];
+            var saveCadena = "";
+            int[] ndata = new int[1000];
+            int dataf = 0;
+            int max = ndata[0];
+            var dpi = "";
+            var gdpi = "";
+            var Sdpi = "";
+            string key = "";
 
             Console.WriteLine("----------------------------------------------------------------------");
             if (GetUsuarios() != null && contador == con)
@@ -100,14 +77,40 @@ namespace LAb02
                             conT = 0;
                             string[] informacion = lineaActual.Split("customers");
                             var u = (informacion[1]);
-                            string[] comp = u.Split('{');
-                            var comp1 = comp[0];
-                            var comp2 = comp[0];
+                            informacion1 = u.Split('{');
+                            var comp1 = informacion1[1];
 
-                            for (int r = 0; r < comp.Length; r++)
+                            for (int r = 1; r < informacion1.Length; r++)
                             {
-                                Console.WriteLine("Datos: " + comp[r]);
+                                saveCadena = informacion1[r].ToString();
+                                Cadena2[r] = saveCadena;
+                                string[] informacion2 = saveCadena.Split('"' + "budget" + '"' + ":");
+                                string[] informacion3 = informacion2[1].Split(',');
+                                var rdata = informacion3[0];
+                                Console.WriteLine(rdata);
+                                dataf = Convert.ToInt32(rdata);
+                                ndata[r - 1] = dataf;
+
+                                if (ndata[r - 1] > max)
+                                {
+                                    max = ndata[r - 1];
+                                }
                             }
+                        }
+                    }
+                    Console.WriteLine("El mayor es: " + max);
+
+                    for (int r = 1; r < informacion1.Length; r++)
+                    {
+                        if (informacion1[r].Contains(max.ToString()))
+                        {
+                            Console.WriteLine("El resultado fue: " + informacion1[r]);
+                            dpi = informacion1[r];
+                            string[] informacion4 = dpi.Split('"' + "dpi" + '"' + ":");
+                            gdpi = informacion4[1];
+                            string[] informacion5 = gdpi.Split(',');
+                            Sdpi = informacion5[0];
+                            Console.WriteLine("El DPI es: " + Sdpi);
                         }
                     }
                 }
@@ -116,27 +119,37 @@ namespace LAb02
 
                     Console.WriteLine("Error" + ex);
                 }
-
+            }
+            if (GetUsuarios2() != null && contador == con)
+            {
+                try
+                {
+                    string allFileData = File.ReadAllText(_path2);
+                    foreach (string lineaActual in allFileData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(lineaActual) && contador == con)
+                        {
+                            conT = 0;
+                            string[] informacion = lineaActual.Split("{");
+                            if (informacion[1].Contains(Sdpi.ToString()))
+                            {
+                                Random signature = new Random();
+                                for (int r = 0; r < 10; r++)
+                                {
+                                    int n = signature.Next(0, 10);
+                                    key += n.ToString();
+                                }
+                                Console.WriteLine("Usuario: " + informacion[1]);
+                                Console.WriteLine("Signature: " + key);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error" + ex);
+                }
             }
         }
     }
 }
-// protected virtual E Find(AVLNode<E> root, E item)
-        //{
-        //    if (root == null)
-        //    {
-        //        return default(E);
-        //    }
-        //    if (item.CompareTo(root.Item) < 0)
-        //    {
-        //        return Find(root.Left, item);
-        //    }
-        //    else
-        //    {
-        //        if (item.CompareTo(root.Item) > 0)
-        //        {
-        //            return Find(root.Right, item);
-        //        }
-        //    }
-        //    return root.Item;
-        //}
